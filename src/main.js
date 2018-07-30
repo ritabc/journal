2ec1145ba103f028466a23bd1ee4deb6ad784d52
Rita Bennett-Chew and Ryan Murry <rita@>
2018-07-30 10:00:31 -0700

    add initial webpack dependencies and basic html and frontend js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#entry-form').submit(function(event){
+    event.preventDefault();
+    var title = $('#title').val();
+    var body = $('#body').val();
+    $('#entries').append("<li><h4>" + title + "</h4>" + body + "</li>");
+  });
+});
