de5f4cbcdb6e038345553df61c3c9a2921efb1c1
Rita Bennett-Chew and Ryan Murry <ryan@>
2018-07-30 14:24:07 -0700

    update file to include logic from entry.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import './styles.css';
+import { Entry } from './entry.js';
 
 $(document).ready(function() {
   $('#entry-form').submit(function(event){
     event.preventDefault();
     var title = $('#title').val();
     var body = $('#body').val();
-    $('#entries').append("<li><h4>" + title + "</h4>" + body + "</li>");
+    var entry = new Entry(body, title);
+    var wordCount = entry.countWords();
+    var letters = entry.countLetters();
+    var teaser = entry.getTeaser();
+    $('#entries').append("<li><h4>" + title + "</h4>" + body + "</br>" + "Number of words: " + wordCount + "</br>" + "This entry has " + letters[0] + " vowels and " + letters[1] + " consonants" + "</br>" + teaser + "</li>");
   });
 });
